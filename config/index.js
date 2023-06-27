9e5821b3b6470dbdd0185b6f73a25cf431e941d3
abayasaye <abay400@gmail.com>
2023-06-27 15:49:31 +0300

    a

--- config/index.js
+++ config/index.js
@@ -1,5 +1,9 @@
-
-const dev =  require('./dev');
-module.exports =dev
-
-
+const prod= require('./prod');
+const dev=  require('./dev');
+console.log(prod);
+console.log(dev)
+if(process.env.NODE_ENV === 'production'){
+    module.exports =prod
+}else{
+    module.exports =dev
+}
